9c5b097f60927f57ad87e9b9af342e95be56ca86
Antti-Henrik Nyberg <antti.nyberg@paf.com>
2019-07-02 13:07:32 +0300

    refactored header passing into php via xhr

--- register.php
+++ register.php
@@ -1,18 +1,26 @@
-<?php // /var/www/html/register.php
+<?php // --> /var/www/html/register.php
   
 require 'vendor/autoload.php';
 use Auth0\SDK\Auth0;
 use josegonzalez\Dotenv\Loader;
 
+// load secrets that are not for version control; if missing, inform how to add
 if (!file_exists('.env')) {
     header($_SERVER['SERVER_PROTOCOL'].' 404 Not Found', true, 404);
-    exit("set environment vars for <a href='https://auth0.com'>Auth0</a>");
+    $information = <<<HTML
+get your application variables from <a href='https://auth0.com'>Auth0</a><br/>
+save them in <code>/var/www/html/.env</code> (see <a href="https://auth0.com/docs/libraries/auth0-php">example</a>)<br/>
+<b>REMEMBER</b> to <code>chown www-data .env</code> and <code>chmod 400 .env</code>
+HTML;
+    exit($information);
 }
+$loader = new Loader([
+  '.env',
+  '.env.default'
+]);
+$loader->parse()->putenv(true);
 
-// load environment vars from outside of version control
-$Dotenv = new Loader('.env');
-$Dotenv->parse()->putenv(true);
-
+// contact authenticator for user information and login if new session is needed
 $auth0 = new Auth0([
   'domain' => getenv('AUTH0_DOMAIN'),
   'client_id' => getenv('AUTH0_CLIENT_ID'),
@@ -20,14 +28,39 @@ $auth0 = new Auth0([
   'redirect_uri' => getenv('AUTH0_REDIRECT_URI'),
   'scope' => 'openid email',
 ]);
-
 $userInfo = $auth0->getUser();
 if (empty($userInfo)) {
     $auth0->login();
-}
+}// else
 
-setcookie('email', $userInfo['email'], time() + 10, '/');
-header("Location: /"); // /?email=".urlencode($userInfo['email']));
+$single_signon_header = getenv('SINGLE_SIGNON_HEADER');
+
+// if email address is OK, use it to sign to QuickBuild; else show Forbidden
+preg_match(getenv('EMAIL_PATTERN'), $userInfo['email'], $matches);
+if (!empty($matches))
+  echo <<<HTML
+    <html>
+      <body>
+        <script language="javascript">
+var xhr = new XMLHttpRequest();
+xhr.open('GET', '/', true); // asynchronous
+xhr.onreadystatechange = function (aEvt) {
+  if (xhr.readyState == 4) {
+    if(xhr.status == 200)
+      window.location = '/';
+     else
+      alert("Error "+xhr.status); //TODO: improve error handling
+  }
+};
+xhr.setRequestHeader('$single_signon_header', '{$matches[0]}');
+xhr.send();
+        </script>
+      <body>
+    </html>
+HTML;
+else
+  header($_SERVER['SERVER_PROTOCOL'].' 403 Forbidden', true, 403);
 
 // https://auth0.com/docs/libraries/auth0-php
+// https://github.com/josegonzalez/php-dotenv
 ?>
